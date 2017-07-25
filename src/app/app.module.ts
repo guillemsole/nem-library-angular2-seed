51dffba4f5e8fa8ea0f76a018955cc61bcad2b00
Aleix Morgadas <aleix602@gmail.com>
2017-07-25 16:25:55 +0200

    site structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MdMenuModule, MdToolbarModule, MdGridListModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +10,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MdMenuModule,
+    MdToolbarModule,
+    MdGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
