36294461cf85d6315690ec9aa74321157212b6f6
Aleix Morgadas <aleix602@gmail.com>
2017-07-30 01:19:28 +0200

    GA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { MaterializeModule } from "angular2-materialize";
 import {BlockchainListener, NEMLibrary, NetworkTypes, UnconfirmedTransactionListener, AccountHttp} from "nem-library";
+import { Angulartics2Module, Angulartics2GoogleAnalytics } from 'angulartics2';
+
 import {BlockchainListenerProvider} from "./providers/BlockchainListenerProvider";
 import {UnconfirmedTransactionListenerProvider} from "./providers/UnconfirmedTransactionListenerProvider";
 import {AccountHttpProvider} from "./providers/AccountHttpProvider";
@@ -19,7 +21,9 @@ NEMLibrary.bootstrap(NetworkTypes.TEST_NET);
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MaterializeModule
+    MaterializeModule,
+
+    Angulartics2Module.forRoot([ Angulartics2GoogleAnalytics ])
   ],
   providers: [
     {provide: BlockchainListener, useFactory: BlockchainListenerProvider},
