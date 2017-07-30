2ab04e85d9ab9df4449714c11685551139954301
Aleix <aleix602@gmail.com>
2017-07-30 18:54:45 +0200

    fix tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,26 @@
-import { TestBed, async } from '@angular/core/testing';
-
-import { AppComponent } from './app.component';
+import {async, TestBed} from '@angular/core/testing';
+import {AppComponent} from './app.component';
+import {Angulartics2GoogleAnalytics, Angulartics2Module} from "angulartics2";
+import {RouterTestingModule} from "@angular/router/testing";
+import {AccountHttpProvider} from "./providers/AccountHttpProvider";
+import {AccountHttp, BlockchainListener, UnconfirmedTransactionListener} from "nem-library";
+import {UnconfirmedTransactionListenerProvider} from "./providers/UnconfirmedTransactionListenerProvider";
+import {BlockchainListenerProvider} from "./providers/BlockchainListenerProvider";
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        Angulartics2Module.forRoot([ Angulartics2GoogleAnalytics ])
+      ],
       declarations: [
         AppComponent
+      ],
+      providers: [
+        {provide: BlockchainListener, useFactory: BlockchainListenerProvider},
+        {provide: UnconfirmedTransactionListener, useFactory: UnconfirmedTransactionListenerProvider},
+        {provide: AccountHttp, useFactory: AccountHttpProvider}
       ],
     }).compileComponents();
   }));
@@ -15,18 +29,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  }));
-
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
   }));
 });
