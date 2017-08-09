286106a84ef5b946fc17dd21d4c86f1bff855b63
Aleix <aleix602@gmail.com>
2017-08-09 20:54:04 +0200

    SOLVED issue with Listeners

--- src/app/providers/BlockchainListenerProvider.ts
+++ src/app/providers/BlockchainListenerProvider.ts
@@ -3,6 +3,8 @@ import {BlockchainListener} from "nem-library";
 // https://pretestnet1.nem.ninja:7891/node/extended-info
 export function BlockchainListenerProvider(): BlockchainListener {
   return new BlockchainListener({
-    domain: "pretestnet1.nem.ninja"
+    protocol: "https",
+    domain: "pretestnet1.nem.ninja",
+    port: 7779
   });
 }
--- src/app/providers/UnconfirmedTransactionListenerProvider.ts
+++ src/app/providers/UnconfirmedTransactionListenerProvider.ts
@@ -1,8 +1,11 @@
 import {UnconfirmedTransactionListener} from "nem-library";
 
 // https://pretestnet1.nem.ninja:7891/node/extended-info
+// https://nis2.wnsl.biz:7779/
 export function UnconfirmedTransactionListenerProvider(): UnconfirmedTransactionListener {
   return new UnconfirmedTransactionListener({
-    domain: "pretestnet1.nem.ninja"
+    protocol: "https",
+    domain: "pretestnet1.nem.ninja",
+    port: 7779
   });
 }
